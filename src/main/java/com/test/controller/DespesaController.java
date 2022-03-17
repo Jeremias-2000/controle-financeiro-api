d5947d784413c698ffc86d94ca0e443b7b35e670
Jeremias-2000 <jdossantospinheiro20@gmail.com>
2022-03-17 09:04:45 -0300

    desenvolvendo os endpoints respectivos as regras de negocio desenvolvidas no service

---
+++ src/main/java/com/test/controller/DespesaController.java
@@ -0,0 +1,30 @@
+package com.test.controller;
+
+import com.test.model.Despesa;
+import com.test.service.DespesaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/controle")
+public class DespesaController {
+
+    @Autowired
+    private DespesaService despesaService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Despesa> returnDespesas(){
+        return despesaService.findDespesas();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Despesa findDespesaById(@PathVariable("id") String id){
+        return despesaService.findDespesaById(id);
+    }
+}
