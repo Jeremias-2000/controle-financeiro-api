f5865432e556eba8c229940b4d54b76bf48d74ef
Jeremias-2000 <jdossantospinheiro20@gmail.com>
2022-03-24 16:44:00 -0300

    adicionando validacao da entidade e o swagger para documentar a api

--- src/main/java/com/test/controller/DespesaController.java
+++ src/main/java/com/test/controller/DespesaController.java
@@ -2,6 +2,10 @@ package com.test.controller;
 
 import com.test.model.Despesa;
 import com.test.service.DespesaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Api(tags = {"Controle"})
 @RequestMapping("/api/v1/controle")
 public class DespesaController {
 
@@ -20,6 +25,12 @@ public class DespesaController {
     @GetMapping("/")
     @Cacheable("despesas")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Retorna as despesas cadastradas")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "requisição executada com sucesso") ,
+            @ApiResponse(code = 400,message = "nenhuma despesa foi encontrada")
+        }
+    )
     public List<Despesa> returnDespesas(){
         return despesaService.findDespesas();
     }
@@ -27,6 +38,12 @@ public class DespesaController {
     @GetMapping("/{id}")
     @Cacheable(value = "despesa",key = "#id")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Retorna uma despesa especifica")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "requisição executada com sucesso") ,
+            @ApiResponse(code = 404,message = "nenhuma despesa foi encontrada com esse id")
+    }
+    )
     public Despesa findDespesaById(@PathVariable("id") String id){
         return despesaService.findDespesaById(id);
     }
--- src/main/java/com/test/model/Despesa.java
+++ src/main/java/com/test/model/Despesa.java
@@ -2,11 +2,21 @@ package com.test.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.yaml.snakeyaml.util.EnumUtils;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -19,9 +29,20 @@ public class Despesa {
 
     @Id
     private String id;
+
+    @NotBlank
     private String descricao;
+
+    @NotNull
     private BigDecimal valor;
+
+    @NotNull
+    @JsonFormat(pattern = "MM/dd/yyyy")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate   data;
+
+    @NotNull
     private Tipo tipo;
 
 
@@ -33,9 +54,6 @@ public class Despesa {
         SAIDA_GASTO_ADICIONAL("gasto_adicional");
 
         private  String descricao;
-
-
-
 
     }
 
